f44a70c2d38cfdfa6cd93a8ca7acbf81a306eab0
Jess <jesswlam@gmail.com>
2016-03-20 14:52:28 -0700

    Created Events class with array for task name, category, start time, end time, and total time.

---
+++ now/now/Events.swift
@@ -0,0 +1,58 @@
+//
+//  Events.swift
+//  now
+//
+//  Created by Jess Lam on 3/20/16.
+//  Copyright © 2016 CP3. All rights reserved.
+//
+
+import Foundation
+
+// Create Task Object
+class Events : NSObject {
+    var taskName :  String
+    var categoryName : String
+    var startTime : Int
+    var endTime : Int
+    var totalTime: Int
+    var completed : Bool
+    
+    init(
+        withTaskName tn : String,
+        categoryName cn : String,
+        startTime st : Int,
+        endTime et : Int,
+        totalTime tt : Int,
+        completed c: Bool
+        ) {
+            taskName = tn
+            categoryName = cn
+            startTime = st
+            endTime = et
+            totalTime = tt
+            completed = c
+}
+
+    // Retrieve value from NS User defaults
+    init(withCoder coder : NSCoder) {
+        taskName = coder.decodeObjectForKey("taskName") as! String
+        categoryName = coder.decodeObjectForKey("categoryName") as! String
+        startTime = coder.decodeObjectForKey("time") as! Int
+        endTime = coder.decodeObjectForKey("time") as! Int
+        totalTime = coder.decodeObjectForKey("time") as! Int
+        completed = coder.decodeObjectForKey("completed") as! Bool
+}
+
+    // Append values to NS User defaults
+    func encodeWithCoder(coder : NSCoder) {
+        coder.encodeObject(taskName, forKey: "taskName")
+        coder.encodeObject(categoryName, forKey: "categoryName")
+        coder.encodeObject(startTime, forKey: "time")
+        coder.encodeObject(endTime, forKey: "time")
+        coder.encodeObject(totalTime, forKey: "time")
+        coder.encodeObject(completed, forKey: "completed")
+    }
+}
+
+
+
